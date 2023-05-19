1a63aa9b7874a8ae44dc54eba59b4cc7a1afd71b
morihagi <mrsk1204@gmail.com>
2023-05-18 19:08:06 -0700

    インラインバリデーションの実装

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -2,4 +2,7 @@ class Cat < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     %w[ id name age ]
   end
+
+  validates :name, presence:true
+  validates :age, presence:true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
