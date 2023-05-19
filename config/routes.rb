5f49787715e5429ce11278df46b4a5ac6df84f93
morihagi <mrsk1204@gmail.com>
2023-05-18 18:51:52 -0700

    Bootstrap5で見た目を整える

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :chicks
   resources :dogs
   resources :cats
+  root 'cats#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
